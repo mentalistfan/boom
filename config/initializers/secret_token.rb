429516295ec7fb977c4a83bf93347809567dd458
mentalistfan <jenya_trishin@yahoo.com>
2014-03-24 20:47:10 +0200

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boom::Application.config.secret_token = '1ba9e7dd2dbd2c43ea039202c24667835ea5b944b47f5242fe7ee8061fb217d00d6550339e7586869806cc7740fc516c222e4ddefb7dfbe0c1a8b4f70a105e8d'
